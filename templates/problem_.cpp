3688e71813a4248c9ac0ca142b988f299d49cf0e
Seongbin Kim <seongbin9786@gmail.com>
2021-02-07 15:23:37 +0900

    updated template & updated status

--- templates/problem_.cpp
+++ templates/problem_.cpp
@@ -2,21 +2,13 @@
 
 /*
 유형: 구현
-문제: [12. ]
-*/
-/*
-[문제 설명]
-
-*/
-/*
 [풀이 전략]
 
 */
 void problem_()
 {
-    int n;
+    int i, n;
     READ_TXT("./resources/input.txt", "rt", stdin);
     scanf("%d", &n);
-    debug("[problem ] n: %d\n", n);
     printf("%d\n", n);
 }
